974bcee078f7fa2161c97f6a51547da08d487636
tvt0102 <144758618+tvt0102@users.noreply.github.com>
2023-10-02 00:47:41 +0700

    codefix1_Tran_Van_Thao

--- button.c
+++ button.c
@@ -8,16 +8,18 @@
 
 #define led 25
 #define button 18
-int buttonMode= 0;
+int buttonMode=0;
 
+void IRAM_ATTR isrFunction() {
+  buttonMode=digitalRead(button);
+}
 void setup(){
     Serial.begin(115200);
     pinMode(led, OUTPUT);
-    pinMode(button, INPUT_PULLUP);
+    pinMode(button, INPUT);
+    attachInterrupt(digitalPinToInterrupt(button), isrFunction, CHANGE);
 }
 void loop(){
-    buttonMode=digitalRead(button);
-    Serial.println(buttonMode);
     if(buttonMode){
         digitalWrite(led, LOW);
         Serial.println("led is off");
@@ -26,5 +28,6 @@ void loop(){
         digitalWrite(led, HIGH);
         Serial.println("led is on");
     }
-    delay(100);
+    delay(1000);
 }
+
